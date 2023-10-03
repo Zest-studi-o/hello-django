bed4312d76afc499de37a745d6189be408643561
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-10-03 20:09:27 +0000

    Coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -7,3 +7,13 @@ class TestModels(TestCase):
         item = Item.objects.create(name='Test Todo Item')
         self.assertFalse(item.done)
 
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertEqual(str(item), 'Test Todo Item')
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
