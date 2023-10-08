f23bddd61c51a7a74621e7cbc763823b541b8baa
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-10-08 22:22:08 +0000

    Changed stuff

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-zeststudio-hellodjango-aw8m26fonam.ws-eu105.gitpod.io',
-    'edrfsw-django-todo-app.herokuapp.com'
+    'os.environ.get('edrfsw-django-todo-app.herokuapp.com')
     ]
 
 
