ac9a4f832d6a6db7e9002887598da50408ab9ea5
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-10-09 10:56:29 +0000

    Change debug back to development

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = development
-DEBUG = True
+DEBUG = development
+#DEBUG = True
 
 ALLOWED_HOSTS = [
     #'8000-zeststudio-hellodjango-aw8m26fonam.ws-eu105.gitpod.io',
